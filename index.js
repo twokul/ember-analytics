9c41da8cf1ab21a2adc4c989571076f350797f6d
Alex Navasardyan <twokul@users.noreply.github.com>
2015-02-23 13:59:28 -0800

    fix for tests and version bump

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var readConfig = require('./lib/config');
 module.exports = {
   name: 'ember-analytics',
 
-  contentFor: function(type) {
-    if (type === 'head') {
+  contentFor: function(type, config) {
+    if (config.environment !== 'test' && type === 'head') {
       return readConfig(this.name);
     }
 
